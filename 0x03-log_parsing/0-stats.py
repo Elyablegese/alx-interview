4c452a3378472861ef65cd14d5abb32491deb606
Elyablegese <101351707+Elyablegese@users.noreply.github.com>
2023-02-20 05:35:43 +0300

    stat ....

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,38 +1,59 @@
 #!/usr/bin/python3
 """
-Write a script that reads stdin line by line and computes metrics
+Module to parse a log file for statistics
+The stdin filestream is read for inputs and metrics are
+computed by python
 """
+
+import re
 import sys
 
-if __name__ == '__main__':
 
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
-    try:
-        for line in sys.stdin:
-            count += 1
-            data = line.split()
-            try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
-    except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+filesize = 0
+errors = ["200", "301", "400", "401", "403", "404", "405", "500"]
+error_count = {}
+ip_re = "\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+         .\b([1-9]|[1-9][0-9]|[1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\b\
+          - "
+datetime_regex = "[\b([1-9][0-9][0-9][0-9])\b-\b([0-9][0-9])\b\
+                  -\b([0-9][0-9])\b \b([0-9][0-9])\b:\b([0-9][0-9])\b:\b\
+                  ([0-9][0-9])\b.\b([0-9][0-9][0-9][0-9][0-9][0-9])\b] "
+url_regex = "\"GET /projects/260 HTTP/1.1\" "
+err_regex = "\b([2][0][0]|[3][0][1]|[4][0][0]|[4][0][1]|[4][0][3]|\
+             [4][0][4]|[4][0][5]|[5][0][0])\b "
+size_regex = "\b([1-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1][0][0-2][0-4])\b"
+line_regex = re.compile(r' '.format(ip_re, datetime_regex,
+                        url_regex, err_regex, size_regex))
+# adjusted the regex, '' and ' ' are different and '' was giving the error
+try:
+    i = 0
+    for line in sys.stdin:
+        is_valid = line_regex.search(line)
+        # print(is_valid)
+        if is_valid:
+            i += 1
+            stats = line.split(' ')
+            # print(stats, len(stats))
+            filesize += int(stats[-1])
+            if error_count.get(stats[-2]):
+                error_count[stats[-2]] += 1
+            else:
+                error_count[stats[-2]] = 1
+            if (i % 10 == 0):
+                print('File size: {}'.format(filesize))
+                for err in errors:
+                    if error_count.get(err):
+                        print('{}: {}'.format(err, error_count.get(err)))
+except KeyboardInterrupt:
+    print('File size: {}'.format(filesize))
+    for err in errors:
+        if error_count.get(err):
+            print('{}: {}'.format(err, error_count.get(err)))
+    raise
+else:
+    print('File size: {}'.format(filesize))
+    for err in errors:
+        if error_count.get(err):
+            print('{}: {}'.format(err, error_count.get(err)))
